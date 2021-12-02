67fa5fa544a7493402f4182c3557c5fa36bacb89
Robert Pelka <123455ff@gmail.com>
2021-12-02 16:22:51 +0100

    Add fetching user data and displaying it in the start view

--- Traffy/Controller/Start/StartViewController.swift
+++ Traffy/Controller/Start/StartViewController.swift
@@ -11,12 +11,24 @@ import Firebase
 class StartViewController: UIViewController {
 
     @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var questionsCounterLabel: UILabel!
+    @IBOutlet weak var signsCounterLabel: UILabel!
+    @IBOutlet weak var questionsProgressView: CustomProgressView!
+    @IBOutlet weak var signsProgressView: CustomProgressView!
+    
     @IBOutlet weak var startLearningButton: UIButton!
     @IBOutlet weak var recognizeSignButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        prepareView()
+        fetchUser()
+    }
+    
+    func prepareView() {
         profileImage.layer.cornerRadius = profileImage.frame.height / 2
         startLearningButton.layer.cornerRadius = 15
         recognizeSignButton.layer.cornerRadius = 15
@@ -24,14 +36,52 @@ class StartViewController: UIViewController {
         recognizeSignButton.layer.borderColor = UIColor.white.cgColor
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func fetchUser() {
+        guard let userID = Auth.auth().currentUser?.uid else {
+            print("DEBUG: Error getting current user id.")
+            return
+        }
+        
+        K.Collections.users.document(userID).addSnapshotListener { snapshot, error in
+            if let error = error {
+                print("DEBUG: Error fetching current user: \(error.localizedDescription)")
+            }
+            
+            let user: User?
+            do {
+                user = try snapshot?.data(as: User.self)
+            }
+            catch let error {
+                print("DEBUG: Error converting snapshot to User type: \(error.localizedDescription)")
+                return
+            }
+            
+            if let user = user {
+                let numberOfQuestions = 342
+                let numberOfSigns = 70
+                
+                self.profileImage.load(url: URL(string: user.profileImageURL))
+                
+                self.usernameLabel.text = "Witaj, \(user.username)!"
+                
+                self.questionsCounterLabel.text = String(user.masteredQuestionsNumber) + " / " +  String(numberOfQuestions)
+                self.signsCounterLabel.text = String(user.discoveredSignsNumber) + " / " +  String(numberOfSigns)
+                
+                let questionsProgress = Float(user.masteredQuestionsNumber) / Float(numberOfQuestions)
+                self.questionsProgressView.setProgress(questionsProgress, animated: true)
+                
+                let signsProgress = Float(user.discoveredSignsNumber) / Float(numberOfSigns)
+                self.signsProgressView.setProgress(signsProgress, animated: true)
+            }
+            
+        }
+        
     }
-    */
-
+    
+    @IBAction func startLearningButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func recognizeSignButtonPressed(_ sender: UIButton) {
+    }
+    
 }
