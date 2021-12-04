a6f4ff2e9794e74fdc8e9e0eda91222b44990ebe
Robert Pelka <123455ff@gmail.com>
2021-12-04 17:10:06 +0100

    Add Question and AnsweredQuestion models

---
+++ Traffy/Model/AnsweredQuestion.swift
@@ -0,0 +1,13 @@
+//
+//  AnsweredQuestion.swift
+//  Traffy
+//
+//  Created by Robert Pelka on 04/12/2021.
+//
+
+import Foundation
+
+struct AnsweredQuestion: Codable {
+    let id: Int
+    let masteryLevel: Int
+}
---
+++ Traffy/Model/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Traffy
+//
+//  Created by Robert Pelka on 04/12/2021.
+//
+
+import Foundation
+
+struct Question: Codable {
+    let id: Int
+    let question: String
+    let image: String
+    let isAbcQuestion: Bool
+    let answerA: String
+    let answerB: String
+    let answerC: String
+    let correctAnswer: String
+}
