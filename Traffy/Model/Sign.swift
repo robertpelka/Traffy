5c83634bc1e1c14c095c46559ed11a6799e6bbfd
Robert Pelka <123455ff@gmail.com>
2021-12-16 17:17:35 +0100

    Add Sign model

---
+++ Traffy/Model/Sign.swift
@@ -0,0 +1,16 @@
+//
+//  Sign.swift
+//  Traffy
+//
+//  Created by Robert Pelka on 16/12/2021.
+//
+
+import Foundation
+
+struct Sign: Codable {
+    let id: String
+    let type: String
+    let name: String
+    let description: String
+    let image: String
+}
